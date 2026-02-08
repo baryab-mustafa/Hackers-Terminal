3b0129517156071436c1f8dff0ad4eba8d6dccc2
baryab-mustafa <baryabmustafa@gmail.com>
2026-02-08 11:58:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,184 @@
+let text = "Initializing Hacking ";
+let text1 = "Initializing Hacking. ";
+let text2 = "Initializing Hacking.. ";
+let text3 = "Initializing Hacking... ";
+let text4 = "Initializing Hacking. ";
+let text5 = "Initializing Hacking.. ";
+let text6 = "Initializing Hacking... ";
+let text7 = `Initializing Hacking... <br>
+             Reading your files `;
+let text8 = `Initializing Hacking... <br>
+             Reading your files. `;
+let text9 = `Initializing Hacking... <br>
+             Reading your files.. `;
+let text10 = `Initializing Hacking... <br>
+             Reading your files... `;
+let text11 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected`;
+let text12 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected.`;
+let text13 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected..`;
+let text14 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected...`;
+let text15 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server`;
+let text16 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server.`;
+let text17 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server..`;
+let text18 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server...`;
+let text19 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server.`;
+let text20 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server..`;
+let text21 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server...`;
+let text22 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server... <br>
+             Cleanning UP`;
+let text23 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server... <br>
+             Cleanning UP.`;
+let text24 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server... <br>
+             Cleanning UP..`;
+let text25 = `Initializing Hacking... <br>
+             Reading your files... <br>
+             Pasword Files Dtected... <br>
+             Sending All Personal Files And Paswords To Server... <br>
+             Cleanning UP...`;
+let text26 = `System Hacked Successfully ✅`;
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML += text;
+}, 2000);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text1;
+}, 2500);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text2;
+}, 2600);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text3;
+}, 2700);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text4;
+}, 2800);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text5;
+}, 2900);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text6;
+}, 3000);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text7;
+}, 3100);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text8;
+}, 3200);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text9;
+}, 3300);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text10;
+}, 3400);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text11;
+}, 3400);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text12;
+}, 3500);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text13;
+}, 3600);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text14;
+}, 3700);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text15;
+}, 3800);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text16;
+}, 3900);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text17;
+}, 4100);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text18;
+}, 4300);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text19;
+}, 4500);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text20;
+}, 4700);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text21;
+}, 4900);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text22;
+}, 5000);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text23;
+}, 5100);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text24;
+}, 5200);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text25;
+}, 5300);
+
+setTimeout(() => {
+  document.querySelector(".div1").innerHTML = text26;
+}, 5500);
